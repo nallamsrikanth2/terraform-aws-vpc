7ec041d40117d07970fbef626099eba0c80847cc
nallamsrikanth12 <nallamsrikanth2@gmail.com>
2026-05-18 21:27:11 +0530

    aws-vpc

--- output.tf
+++ output.tf
@@ -26,6 +26,11 @@ output "database_subnet_ids" {
   
 }
 
+output "database_subnet_group" {
+    value = aws_db_subnet_group.default[*].id
+  
+}
+
 output "igw_id" {
     value = aws_internet_gateway.igw.id
   
